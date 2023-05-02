a3c16775e663f117f41e0c4353dad817187064e7
user <gafurov.daniel28@gmail.com>
2023-05-02 14:09:13 +0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.a = 6;
+        triangle.b = 5;
+        triangle.c = 8;
+        System.out.println((triangle.a+ triangle.b+ triangle.c)/2);
+        triangle.area();
+
+
+
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+
+    public void area(){
+        System.out.println((a+b+c)/2+" area of triangle");
+
+    }
+
+}
